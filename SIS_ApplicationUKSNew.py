729e438afd955db948cc05aa41bfed1df8f49147
karlpetricdr-art <karl.petricdr@gmail.com>
2026-02-06 13:54:54 +0100

    Update SIS_ApplicationUKSNew.py
    Back

--- SIS_ApplicationUKSNew.py
+++ SIS_ApplicationUKSNew.py
@@ -390,7 +390,7 @@ with st.sidebar:
     
     api_key = st.text_input(
         "Groq API Key:", 
-        type="passsword", 
+        type="password", 
         help="Security: Your key is held only in volatile RAM and is never stored on our servers."
     )
     
@@ -654,5 +654,6 @@ if st.button("🚀 Execute Multi-Dimensional Synthesis", use_container_width=Tru
 # PODNOŽJE (ZAHVALA IN VERZIJA)
 st.divider()
 st.caption("SIS Universal Knowledge Synthesizer | v21.0 Human Thinking Metamodel Architecture | 2026")
+
 
 
